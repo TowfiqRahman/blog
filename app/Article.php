75533f6441acf4a7600b60706b88375ed17149d7
TowfiqRahman <towfiq.cse.cuet@gmail.com>
2020-07-29 02:02:40 +0600

    31 created tags and articls_tag tabel for manytomany relationship

--- app/Article.php
+++ app/Article.php
@@ -11,4 +11,8 @@ class Article extends Model
     public function author(){
       return $this->belongsTo(User::class,'user_id');
     }
+
+    public function tags(){
+      return $this->belongsToMany(Tag::class,);
+    }
 }
